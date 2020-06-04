c52bc7b68d5d5bcdcaef6d275dd046ea745ae229
Wani Joseph <waynekhalifa.wk@gmail.com>
2020-06-04 09:18:47 +0200

    styles my account

--- my-account.php
+++ my-account.php
@@ -17,21 +17,27 @@ if (isset($_GET['product'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo DIR . 'assets/css/styles.css'; ?>" type="text/css">
+  <!-- google font -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
   <title>My Account | <?php echo SITETITLE; ?></title>
 </head>
 
 <body>
-  <header>
-    <div>
-      <a href="<?php echo DIR; ?>"><?php echo SITETITLE; ?></a>
+  <div class="header-wrapper">
+    <div class="container">
+      <header class="site-header">
+        <a class="logo" href="<?php echo DIR; ?>"><?php echo SITETITLE; ?></a>
+        <ul class="navigation">
+          <li><a href="<?php echo DIR . '?logout'; ?>">logout</a></li>
+        </ul>
+      </header>
     </div>
-    <ul>
-      <li><a href="<?php echo DIR . '?logout'; ?>">logout</a></li>
-    </ul>
-  </header>
-  <main>
+  </div>
+  <main class="container">
     <?php messages() ?>
-    <h1>Hi <?php echo $_SESSION['auth_user']['name'] ?></h1>
+    <h1 class="page-title">Hi <?php echo $_SESSION['auth_user']['name'] ?> welcome back</h1>
+    <h2 class="section-title">Your orders history</h2>
     <?php
     $user_id = $_SESSION['auth_user']['id'];
     // displays user orders
@@ -42,18 +48,22 @@ if (isset($_GET['product'])) {
     $sql = mysqli_query($conn, $query);
     $result = mysqli_num_rows($sql);
     if ($result > 0) {
+      echo "<ul class='orders'>";
       while ($row = mysqli_fetch_object($sql)) :
-        echo "<li>";
+        echo "<li class='order'>";
         echo $row->name;
         echo "</li>";
       endwhile;
+      echo "</ul>";
     } else {
       echo "<p>you didn't purchase any product yet!<p>";
     }
     ?>
-    <a href="<?php echo DIR; ?>">continue shopping</a>
+    <a class="primary-btn" href="<?php echo DIR; ?>">continue shopping</a>
   </main>
-  <footer>footer</footer>
+  <footer class="site-footer">
+    <p>&copy; <?php echo date('Y'); ?> Simple Shop. All rights reserved.</p>
+  </footer>
 </body>
 
 </html>
