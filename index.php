fda7255743678e26692789e6c9cde01b323f3956
Wani Joseph <waynekhalifa.wk@gmail.com>
2020-06-04 08:59:05 +0200

    styles single product

--- index.php
+++ index.php
@@ -13,29 +13,34 @@ if (isset($_GET['logout'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo DIR . 'assets/css/styles.css'; ?>" type="text/css">
+  <!-- google font -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
   <title><?php echo SITETITLE; ?></title>
 </head>
 
 <body>
 
-  <header>
-    <div>
-      <a href="<?php echo DIR; ?>"><?php echo SITETITLE; ?></a>
+  <div class="header-wrapper">
+    <div class="container">
+      <header class="site-header">
+        <a class="logo" href="<?php echo DIR; ?>"><?php echo SITETITLE; ?></a>
+        <ul class="navigation">
+          <?php if (logged_in()) : ?>
+            <li><a href="<?php echo DIR . 'my-account.php'; ?>">my account</a></li>
+            <li><a href="<?php echo DIR . '?logout'; ?>">logout</a></li>
+          <?php else : ?>
+            <li><a href="<?php echo DIR . 'login.php'; ?>">login</a></li>
+            <li><a href="<?php echo DIR . 'register.php'; ?>">register</a></li>
+          <?php endif; ?>
+          <?php if (admin()) : ?>
+            <li><a href="<?php echo DIRADMIN; ?>">admin dashboard</a></li>
+          <?php endif; ?>
+        </ul>
+      </header>
     </div>
-    <ul>
-      <?php if (logged_in()) : ?>
-        <li><a href="<?php echo DIR . 'my-account.php'; ?>">my account</a></li>
-        <li><a href="<?php echo DIR . '?logout'; ?>">logout</a></li>
-      <?php else : ?>
-        <li><a href="<?php echo DIR . 'login.php'; ?>">login</a></li>
-        <li><a href="<?php echo DIR . 'register.php'; ?>">register</a></li>
-      <?php endif; ?>
-      <?php if (admin()) : ?>
-        <li><a href="<?php echo DIRADMIN; ?>">admin dashboard</a></li>
-      <?php endif; ?>
-    </ul>
-  </header>
-  <main>
+  </div>
+  <main class="container">
     <?php
     // review product
     if (isset($_POST['submit'])) {
@@ -52,7 +57,16 @@ if (isset($_GET['logout'])) {
       $sql = mysqli_query($conn, "SELECT * FROM products WHERE id='$id'");
       $product = mysqli_fetch_object($sql);
 
-      echo "<h1>" . $product->name . "</h1>";
+      echo "<h1 class='page-title'>" . $product->name . "</h1>";
+      echo "<div class='product-page'>";
+      echo "<img src='" . DIR . "assets/images/" . $product->image . "' alt='" . $product->name . "' />";
+      echo "<div class='product-content'>";
+      echo "<p class='product-name'>" . $product->name . "</p>";
+      echo "<p class='product-desc'>" . $product->description . "</p>";
+      echo "<p class='product-price'>$" . $product->price . "</p>";
+      echo "<a class='purchase' href='" . DIR . "my-account.php?product=" . $product->id . "'>purchase</a>";
+      echo "</div>";
+      echo "</div>";
 
       // list all products reviews
       $query = "SELECT * FROM reviews
@@ -61,11 +75,11 @@ if (isset($_GET['logout'])) {
       WHERE reviews.product_id = '$id'";
 
       $sql = mysqli_query($conn, $query);
-      echo "<ul>";
+      echo "<ul class='reviews'>";
       while ($row = mysqli_fetch_object($sql)) :
-        echo "<li>";
-        echo "<p>" . $row->name. "</p>";
-        echo "<p>" . $row->comment. "</p>";
+        echo "<li class='review'>";
+        echo "<p class='reviewer'>" . $row->name . "</p>";
+        echo "<p class='comment'>" . $row->comment . "</p>";
         echo "</li>";
       endwhile;
       echo "</ul>";
@@ -75,7 +89,7 @@ if (isset($_GET['logout'])) {
       if (logged_in()) : ?>
         <form method="post" action="<?= $_SERVER['PHP_SELF'] ?>">
           <input type="hidden" name="product" value="<?php echo $id; ?>">
-          <div>
+          <div class="form-control">
             <label for="review">Review Product</label>
             <textarea name="review" cols="30" rows="10">
           </textarea>
@@ -88,12 +102,18 @@ if (isset($_GET['logout'])) {
       endif;
     else :
       // list all products
-      echo "<ul>";
+      echo "<ul class='products'>";
       $sql = mysqli_query($conn, "SELECT * FROM products");
       while ($row = mysqli_fetch_object($sql)) :
-        echo "<li>";
-        echo "<a href='" . DIR . "?product=" . $row->id . "'>" . $row->name . "</a>";
-        echo "<a href='" . DIR . "my-account.php?product=" . $row->id . "'>purchase</a>";
+        echo "<li class='product'>";
+        echo "<a href='" . DIR . "?product=" . $row->id . "'>";
+        echo "<img src='" . DIR . "assets/images/" . $row->image . "' alt='" . $row->name . "' />";
+        echo "</a>";
+        echo "<div class='product-meta'>";
+        echo "<p class='product-name'>" . $row->name . "</p>";
+        echo "<p class='product-price'>$" . $row->price . "</p>";
+        echo "<a class='purchase' href='" . DIR . "my-account.php?product=" . $row->id . "'>purchase</a>";
+        echo "</div>";
         echo "</li>";
       endwhile;
       echo "</ul>";
@@ -101,7 +121,9 @@ if (isset($_GET['logout'])) {
     ?>
   </main>
 
-  <footer>footer</footer>
+  <footer class="site-footer">
+    <p>&copy; <?php echo date('Y'); ?> Simple Shop. All rights reserved.</p>
+  </footer>
 </body>
 
 </html>
